fe866b66d2be032bbe3df43b0e0ba78db548e9ec
Piyush <44617356+piyush-shandilya@users.noreply.github.com>
2020-10-01 23:21:13 +0530

    signup feature implemented with all the validators

---
+++ backend/functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBzyVWP7lDTu2J_X-gwqrS_5HZ5aFvWvPM",
+  authDomain: "social-ape-68007.firebaseapp.com",
+  databaseURL: "https://social-ape-68007.firebaseio.com",
+  projectId: "social-ape-68007",
+  storageBucket: "social-ape-68007.appspot.com",
+  messagingSenderId: "908845232998",
+  appId: "1:908845232998:web:ae0be04f93bd97170ccea7",
+  measurementId: "G-0GZFYP7Y8X",
+};
