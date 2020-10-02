576b08b13fc02b4c96163a5fb1b9602ec7798a19
Piyush <44617356+piyush-shandilya@users.noreply.github.com>
2020-10-02 23:12:55 +0530

    getting user details of an authenticated user

--- backend/functions/routes/user/index.js
+++ backend/functions/routes/user/index.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const { addUserDetails } = require("../../handlers/user/addUserDetails");
 const FBauth = require("../../utils/FBauth");
+const {
+  getAuthenticatedUser,
+} = require("../../handlers/user/getAuthenticatedUser");
 
 const route = Router();
 
@@ -8,5 +11,6 @@ route.use("/signup", require("./signup"));
 route.use("/login", require("./login"));
 route.use("/uploadImage", require("./uploadImage"));
 route.post("/", FBauth, addUserDetails);
+route.get("/", FBauth, getAuthenticatedUser);
 
 module.exports = route;
