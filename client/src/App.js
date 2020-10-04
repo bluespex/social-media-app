ce1815c58cfc383739518d6d2c4b48ec8a548e04
Piyush <44617356+piyush-shandilya@users.noreply.github.com>
2020-10-04 11:34:03 +0530

    frontend initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+//components
+import Navbar from "./components/Navbar";
+
+//Pages
+import home from "./pages/home";
+import signup from "./pages/signup";
+import login from "./pages/login";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/signup" component={signup} />
+              <Route exact path="/login" component={login} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
